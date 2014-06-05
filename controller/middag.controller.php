88265a936d96b5b19f31e1f218a61188d22be9a1
Marius Mandal <mariusmandal@gmail.com>
2014-06-05 13:56:07 +0200

    riktig lenke for nedlasting

--- controller/middag.controller.php
+++ controller/middag.controller.php
@@ -45,19 +45,22 @@ $objPHPExcel = null;
 exInit('Ledermiddag');
 exSheetName('Gjester');
 
-$objPHPExcel->setActiveSheetIndex(0);
-$rad = 1;
-excell('A'.$rad, 'Ankomsttid','bold');
+excell('A1', 'Fylke', 'bold');
+excell('B1', 'Navn', 'bold');
+excell('C1', 'Mobil', 'bold');
+excell('D1', 'E-post', 'bold');
+excell('E1', 'Prisgruppe', 'bold');
 
 $rad = 1;
 foreach( $ledere as $leder ) {
 	$rad++;
-/*	excell('A'.$rad, $leder->fylke);
-	excell('B'.$rad, utf8_encode($leder->l_navn));
+	excell('A'.$rad, $leder->fylke);
+	excell('B'.$rad, $leder->l_navn);
 	excell('C'.$rad, $leder->l_mobilnummer);
 	excell('D'.$rad, $leder->l_epost);
-	excell('E'.$rad, $leder->gratis ? 'Gratis' : 'Betalt');*/
+	excell('E'.$rad, $leder->gratis ? 'Gratis' : 'Betalt');
 }
-$TWIG['excel_middag'] = exWrite($objPHPExcel,'UKMF_Ledermiddag_UKMFestivalen');
+exWrite($objPHPExcel,'UKMF_Ledermiddag_UKMFestivalen');
+$TWIG['excel_middag'] = 'http://download.ukm.no/phpexcel/UKMF_Ledermiddag_UKMFestivalen.xlsx';
 
 $TWIG['ledere'] = $ledere;
