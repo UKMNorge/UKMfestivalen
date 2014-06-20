98d5273218bd4035a36253d86b9859568abaa85e
Marius Mandal <mariusmandal@gmail.com>
2014-06-20 11:12:23 +0200

    trygge filnavn

--- controller/sveveeksport.controller.php
+++ controller/sveveeksport.controller.php
@@ -39,7 +39,7 @@ foreach($innslag as $band_type => $bands) {
 	
 	$excelData = new stdClass();
 	$excelData->name = $bt[$band_type];
-	$excelData->url = exWrite($objPHPExcel, 'UKMF_Sveveksport_'.$excelData->name);
+	$excelData->url = exWrite($objPHPExcel, 'UKMF_Sveveksport_'.$band_type_name);
 	
 	$TWIG['excel'][] = $excelData;
 }
