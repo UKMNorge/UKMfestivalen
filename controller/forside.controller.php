266b06e577ad704db4b60dd4a4754bfc797ef409
Marius Mandal <mariusmandal@gmail.com>
2015-05-17 21:24:31 +0200

    Kontrollerer visning av workshops

--- controller/forside.controller.php
+++ controller/forside.controller.php
@@ -3,7 +3,11 @@ require_once('UKM/monstring.class.php');
 
 
 if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-	$options = array('vis_deltakerinfo_mode_pre','vis_festivalinfo_forside_mode_pre','vis_festivalinfo_meny_mode_pre');
+	$options = array('vis_deltakerinfo_mode_pre',
+					 'vis_festivalinfo_forside_mode_pre',
+					 'vis_festivalinfo_meny_mode_pre',
+					 'vis_workshops_forside_mode_pre',
+					 'vis_workshopsinfo_forside_mode_pre');
 	
 	foreach( $options as $post ) {
 		if( isset( $_POST[ $post ] ) ) {
@@ -24,6 +28,8 @@ $mode_pre = new stdClass();
 $mode_pre->vis_deltakerinfo = get_option('vis_deltakerinfo_mode_pre');
 $mode_pre->vis_festivalinfo_forside = get_option('vis_festivalinfo_forside_mode_pre');
 $mode_pre->vis_festivalinfo_meny = get_option('vis_festivalinfo_meny_mode_pre');
+$mode_pre->vis_workshops_forside = get_option('vis_workshops_forside_mode_pre');
+$mode_pre->vis_workshopsinfo_forside = get_option('vis_workshopsinfo_forside_mode_pre');
 $TWIG['modes']['pre'] = $mode_pre;
 
 
