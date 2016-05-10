e5835d754cad8849490f19e6936c2cd68a807a7d
Marius Mandal <mariusmandal@gmail.com>
2016-05-10 13:59:16 +0200

    Kontaktpersoner som valg

--- controller/forside.controller.php
+++ controller/forside.controller.php
@@ -8,7 +8,8 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 					 'vis_festivalinfo_meny_mode_pre',
 					 'vis_workshops_meny_mode_pre',
 					 'vis_workshops_forside_mode_pre',
-					 'vis_workshopsinfo_forside_mode_pre');
+					 'vis_workshopsinfo_forside_mode_pre',
+					 'vis_festivalinfo_kontaktpersoner');
 	
 	foreach( $options as $post ) {
 		if( isset( $_POST[ $post ] ) ) {
@@ -32,7 +33,10 @@ $mode_pre->vis_festivalinfo_meny = get_option('vis_festivalinfo_meny_mode_pre');
 $mode_pre->vis_workshops_meny = get_option('vis_workshops_meny_mode_pre');
 $mode_pre->vis_workshops_forside = get_option('vis_workshops_forside_mode_pre');
 $mode_pre->vis_workshopsinfo_forside = get_option('vis_workshopsinfo_forside_mode_pre');
+$mode_pre->vis_festivalinfo_kontaktpersoner = get_option('vis_festivalinfo_kontaktpersoner');
+
+
 $TWIG['modes']['pre'] = $mode_pre;
 
-
+$TWIG['UKM_HOSTNAME'] = UKM_HOSTNAME;
 $TWIG['monstring'] = $monstring;
